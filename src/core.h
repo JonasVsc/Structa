4bbbc0da2a5c1005bc09023cc1667ac13e15df5b
Jonas Vasconcelos <jonasvasconcelosbarbosa@gmail.com>
2025-06-20 16:25:44 -0300

    refactor: select GPU & create device

--- src/core.h
+++ src/core.h
@@ -7,6 +7,7 @@ typedef enum StResult {
 	ST_ERROR_INCOMPLETE_CREATE_INFO = -2,
 	ST_ERROR_OUT_OF_MEMORY = -3,
 	ST_ERROR_SDL = -4,
+	ST_ERROR_SUPPORTED_GPU_NOT_FOUND = -5
 	// other errors / warnings
 } StResult;
 
--- src/renderer.c
+++ src/renderer.c
@@ -36,9 +36,28 @@ typedef struct StWindow {
 	bool close;
 } StWindow;
 
+typedef struct StQueueFamilyIndices {
+	uint32_t graphicsFamily;
+	uint32_t presentFamily;
+} StQueueFamilyIndices;
+
+typedef struct StGPU {
+	VkPhysicalDevice handle;
+	StQueueFamilyIndices familyIndices;
+} StGPU;
+
+typedef struct StDevice {
+	VkDevice handle;
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+} StDevice;
+
 typedef struct StRenderer_T {
 	StWindow window;
 	VkInstance instance;
+	VkSurfaceKHR surface;
+	StGPU GPU;
+	StDevice device;
 } StRenderer_T;
 
 // =============================================================================
@@ -47,10 +66,17 @@ typedef struct StRenderer_T {
 
 static StResult createWindow(const StWindowCreateInfo* createInfo, StWindow* window);
 static StResult createInstance(StWindow window, VkInstance* instance);
+static StResult selectGPU(VkInstance instance, VkSurfaceKHR surface, StGPU* GPU);
+static StResult createDevice(StGPU gpu, StDevice* device);
 
 static void destroyWindow(StRenderer renderer);
 static void destroyInstance(StRenderer renderer);
+static void destroySurface(StRenderer renderer);
+static void destroyDevice(StRenderer renderer);
 
+static StResult findQueueFamilies(VkPhysicalDevice gpu, VkSurfaceKHR surface, StQueueFamilyIndices* queueFamilyIndices);
+
+// callbacks
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
 
 // =============================================================================
@@ -67,6 +93,9 @@ StResult stCreateRenderer(const StRendererCreateInfo* createInfo, StRenderer* re
 
 	createWindow(createInfo->windowCreateInfo, &(*renderer)->window);
 	createInstance((*renderer)->window, &(*renderer)->instance);
+	SDL_Vulkan_CreateSurface((*renderer)->window.handle, (*renderer)->instance, &(*renderer)->surface);
+	selectGPU((*renderer)->instance, (*renderer)->surface, &(*renderer)->GPU);
+	createDevice((*renderer)->GPU, &(*renderer)->device);
 
 	return ST_SUCCESS;
 }
@@ -78,6 +107,8 @@ void stDestroyRenderer(StRenderer renderer)
 		return;
 	}
 
+	destroyDevice(renderer);
+	destroySurface(renderer);
 	destroyInstance(renderer);
 	destroyWindow(renderer);
 
@@ -115,7 +146,7 @@ void stPollEvents(StRenderer renderer)
 // Helpers Implementation
 // =============================================================================
 
-StResult createWindow(const StWindowCreateInfo* createInfo, StWindow* window)
+static StResult createWindow(const StWindowCreateInfo* createInfo, StWindow* window)
 {
 	window->handle = SDL_CreateWindow("teste", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_VULKAN);
 	if (window->handle == NULL)
@@ -157,7 +188,7 @@ static StResult createInstance(StWindow window, VkInstance* instance)
 		}
 		if (!hasSupport)
 		{
-			return ST_ERROR;
+			return ST_ERROR_SUPPORTED_GPU_NOT_FOUND;
 		}
 	}
 
@@ -202,7 +233,74 @@ static StResult createInstance(StWindow window, VkInstance* instance)
 	return ST_SUCCESS;
 }
 
-void destroyWindow(StRenderer renderer)
+static StResult selectGPU(VkInstance instance, VkSurfaceKHR surface, StGPU* GPU)
+{
+	uint32_t GPUCount = 0;
+	vkEnumeratePhysicalDevices(instance, &GPUCount, NULL);
+	
+	if (GPUCount == 0)
+	{
+		return ST_ERROR_SUPPORTED_GPU_NOT_FOUND;
+	}
+
+	VkPhysicalDevice GPUs[10] = { 0 };
+	vkEnumeratePhysicalDevices(instance, &GPUCount, GPUs);
+
+	for (uint32_t GPUIdx = 0; GPUIdx < GPUCount; ++GPUIdx)
+	{
+		StQueueFamilyIndices queueFamilyIndices = { 0 };
+		if (findQueueFamilies(GPUs[GPUIdx], surface, &queueFamilyIndices) == ST_SUCCESS)
+		{
+			(*GPU).handle = GPUs[GPUIdx];
+			(*GPU).familyIndices = queueFamilyIndices;
+			return ST_SUCCESS;
+		}
+	}
+
+	return ST_ERROR_SUPPORTED_GPU_NOT_FOUND;
+}
+
+static StResult createDevice(StGPU GPU, StDevice* device)
+{
+	bool isExclusive = GPU.familyIndices.graphicsFamily == GPU.familyIndices.presentFamily;
+
+	float queuePriority = 1.0f;
+	VkDeviceQueueCreateInfo graphicsQueueCI = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = GPU.familyIndices.graphicsFamily,
+		.queueCount = 1,
+		.pQueuePriorities = &queuePriority
+	};
+
+	VkDeviceQueueCreateInfo presentQueueCI = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = GPU.familyIndices.graphicsFamily,
+		.queueCount = 1,
+		.pQueuePriorities = &queuePriority
+	};
+
+	VkDeviceQueueCreateInfo queuesCI[2] = { graphicsQueueCI, presentQueueCI };
+	VkPhysicalDeviceFeatures features = { 0 };
+	const char* deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	VkDeviceCreateInfo deviceCI = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.queueCreateInfoCount = isExclusive ? (uint32_t)1 : (uint32_t)2,
+		.pQueueCreateInfos = isExclusive ? &graphicsQueueCI : queuesCI,
+		.enabledExtensionCount = 1,
+		.ppEnabledExtensionNames = &deviceExtensions,
+		.pEnabledFeatures = NULL
+	};
+
+	VK_CHECK(vkCreateDevice(GPU.handle, &deviceCI, NULL, &device->handle));
+
+	vkGetDeviceQueue(device->handle, GPU.familyIndices.graphicsFamily, 0, &device->graphicsQueue);
+	vkGetDeviceQueue(device->handle, GPU.familyIndices.presentFamily, 0, &device->presentQueue);
+
+	return ST_SUCCESS;
+}
+
+static void destroyWindow(StRenderer renderer)
 {
 	if (renderer->window.handle == NULL)
 	{
@@ -212,7 +310,7 @@ void destroyWindow(StRenderer renderer)
 	SDL_DestroyWindow(renderer->window.handle);
 }
 
-void destroyInstance(StRenderer renderer)
+static void destroyInstance(StRenderer renderer)
 {
 	if (renderer->instance == NULL)
 	{
@@ -220,6 +318,66 @@ void destroyInstance(StRenderer renderer)
 	}
 
 	vkDestroyInstance(renderer->instance, NULL);
+}
+
+static void destroySurface(StRenderer renderer)
+{
+	if (renderer->surface == NULL)
+	{
+		return;
+	}
+
+	vkDestroySurfaceKHR(renderer->instance, renderer->surface, NULL);
+}
+
+static void destroyDevice(StRenderer renderer)
+{
+	if (renderer->device.handle == NULL)
+	{
+		return;
+	}
+
+	vkDestroyDevice(renderer->device.handle, NULL);
+}
+
+static StResult findQueueFamilies(VkPhysicalDevice gpu, VkSurfaceKHR surface, StQueueFamilyIndices* queueFamilyIndices)
+{
+	queueFamilyIndices->graphicsFamily = UINT32_MAX;
+	queueFamilyIndices->presentFamily = UINT32_MAX;
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queueFamilyCount, NULL);
+
+	if (queueFamilyCount == 0)
+	{
+		return;
+	}
+
+	VkQueueFamilyProperties queueFamilies[20] = { 0 };
+	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queueFamilyCount, queueFamilies);
+	
+	for (uint32_t qIdx = 0; qIdx < queueFamilyCount; ++qIdx)
+	{
+		VkBool32 presentSupport = VK_FALSE;
+		vkGetPhysicalDeviceSurfaceSupportKHR(gpu, qIdx, surface, &presentSupport);
+
+		if (presentSupport == VK_TRUE)
+		{
+			queueFamilyIndices->presentFamily = qIdx;
+		}
+
+		if (queueFamilies[qIdx].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			queueFamilyIndices->graphicsFamily = qIdx;
+		}
+
+		if (queueFamilyIndices->presentFamily != UINT32_MAX && queueFamilyIndices->graphicsFamily != UINT32_MAX)
+		{
+			return ST_SUCCESS;
+		}
+	}
+
+	return ST_ERROR;
 }
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
